2a5f472df5ca59f78c7df1b89b453720bdcbe769
daywabit <agkhantphyo555@gmail.com>
2026-05-15 14:55:44 +0000

    Deploying to gh-pages from @ daywabit/daywa619@0941d65bb82f85ad00250b669e07efdcee587001 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/daywa/sw.js', { scope: '/daywa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/daywa619/sw.js', { scope: '/daywa619/' })})}
